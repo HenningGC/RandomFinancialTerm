3929abc9bfc323ffdc8fdac94f807bc7c54252ad
Henning Gruhl <55255975+HenningGC@users.noreply.github.com>
2020-01-20 12:05:42 +0100

    Update InvestopediaBot.py

--- InvestopediaBot.py
+++ InvestopediaBot.py
@@ -4,6 +4,7 @@ import time
 from time import sleep
 import random
 
+
 driver = webdriver.Chrome()
 driver.get('https://www.investopedia.com/financial-term-dictionary-4769738')
 
@@ -11,22 +12,16 @@ webs = []
 page_digits = 7
 page_num = random.randrange(4769351,4769377)
 page_num = str(page_num)
+term_num = random.randrange(1,280)
+term_num = str(term_num)
 
 url1 = ("https://www.investopedia.com/terms-beginning-with-a-") + page_num
 driver.get(url1)
 
-html = driver.find_element_by_id('dictionary-top300-list__list-content_1-0').get_attribute("innerHTML")[10:65]
-
-webs.append(html)
-webs_cleaned = ([s.strip('" id="diction') for s in webs])
-
-print(webs_cleaned)
-
-listToStr =' '.join([str(i) for i in webs_cleaned])
-print(listToStr)
-time.sleep(1)
-driver.get(listToStr)
+elems = driver.find_element_by_id("dictionary-top300-list__list_1-0-" + term_num).get_attribute("href")
+time.sleep(0.5)
+driver.get(elems)
 text = driver.find_element_by_id('article_1-0').get_attribute("innerText")
 print(text)
 driver.close()
-input('Press Enter to exit')
+input('Press ENTER to exit')
